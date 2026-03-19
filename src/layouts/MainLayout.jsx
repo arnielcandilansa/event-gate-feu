e66b542a7b87efd1b120088b0a18ddf8fc202f96
Arniel Candilansa <arniel.candilansa@gmail.com>
2026-03-19 14:55:34 +0800

    added footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const MainLayout = ({ children }) => {
 	return (
-		<div>
+		<div className="flex flex-col min-h-screen">
 			<NavBar />
-			<main className="max-w-7xl w-full mx-auto">{children}</main>
+			<main className="max-w-7xl w-full mx-auto flex-1">{children}</main>
+			<Footer />
 		</div>
 	);
 };
